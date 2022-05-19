8b1c3ecc1400561e7b1f9fa2f30db8cab5a0e160
Pedro Medeiros <pedrotomemedeiros@gmail.com>
2022-05-19 09:09:40 +0000

    First commit

---
+++ android/app/src/main/kotlin/com/example/eprosec/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.eprosec
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/news.dart
@@ -0,0 +1,12 @@
+class News {
+  String date;
+  String title;
+  String text;
+  String image;
+
+  News(
+      {required this.title,
+      required this.date,
+      required this.text,
+      required this.image});
+}
